623ae0c69dbc30b95cb6d62e34acc07d7f1f7962
Fahira <fahiratasla13@gmail.com>
2021-09-30 02:39:10 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/login_signup/Mulai.java
@@ -0,0 +1,16 @@
+package com.example.login_signup;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Mulai extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mulai);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/login_signup/Splash_Screen.java
@@ -0,0 +1,15 @@
+package com.example.login_signup;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+    }
+}
